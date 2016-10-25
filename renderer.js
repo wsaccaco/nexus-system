b15c9969398a8d45f6399efa79ff0e7aa18dabc7
Waldo J <w.saccaco@gmail.com>
2016-10-25 00:52:06 -0500

    init proyect

--- renderer.js
+++ renderer.js
@@ -1,3 +1,77 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {remote} = require('electron');
+const angular = require('angular');
+require('angular-material');
+require('angular-aria');
+require('angular-animate');
+
+
+var userApp = angular.module('userApp', ['ngMaterial']);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+userApp.controller('UserControllerShow', function UserControllerShow($scope) {
+    $scope.phones = [
+        {
+            name: 'Nexus S',
+            snippet: 'Fast just got faster with Nexus S.'
+        }, {
+            name: 'Motorola XOOM™ with Wi-Fi',
+            snippet: 'The Next, Next Generation tablet.'
+        }, {
+            name: 'MOTOROLA XOOM™',
+            snippet: 'The Next, Next Generation tablet.'
+        }
+    ];
+}).controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.toggleLeft = buildDelayedToggler('left');
+
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+
+            return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+            return debounce(function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }, 200);
+        }
+
+    })
+    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav('left').close()
+                .then(function () {
+                    $log.debug("close LEFT is done");
+                });
+
+        };
+    })
+
+
+const {app, Menu, BrowserWindow} = remote
+
